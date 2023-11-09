64c3bbc00e9f59317d979350d780ba96424ff715
luisanton-io <hello@luisanton.io>
2023-11-10 00:51:40 +0400

    tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+describe("App 1", () => {
+  test("that the app renders", () => {
+    render(<App />);
+  });
+});
+
+describe("App 2", () => {
+  test("that true is actually true", () => {
+    expect(true).toBe(true);
+  });
+
+  test("that the app has a navbar with our name", () => {
+    render(<App />);
+    const AppLogo = screen.getByText("EPICODE Products");
+    expect(AppLogo).toBeInTheDocument();
+  });
 });
